80a4107da4847a93e801758978ea07c2f7386698
Ujjwal Gupta <guptaujjwal600@gmail.com>
2023-05-01 09:32:54 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+// creating a simple navbar
+const Navbar = () => {
+  return (
+    <div className={styles.nav}>
+      <h1> React Calculator</h1>
+    </div>
+  );
+};
+
+export default Navbar;
